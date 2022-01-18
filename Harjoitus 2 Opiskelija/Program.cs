ebc3ab9ef0bafaf354a490078198533cb5e8216d
3DW1Z4RD <97434008+EDlS4l@users.noreply.github.com>
2022-01-18 12:55:56 +0200

    Harjoitus 2

---
+++ Harjoitus 2 Opiskelija/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Opiskelija_harjoitus_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            opiskelija Opiskelija = new opiskelija("Matti", "132409A4442", 21);
+            Opiskelija.TulostaData();
+            Opiskelija.MuokkaaOpintopisteitä(21);
+            Opiskelija.TulostaData();
+        }
+    }
+}
