25fd9d23d8a68ad8a66af4be209a4c01feb1dacf
3DW1Z4RD <97434008+EDlS4l@users.noreply.github.com>
2022-01-24 10:56:15 +0200

    Harjoitus 3

---
+++ Kiuas harjoitus 3/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Kiuas_harjoitus_3
+{
+    class Kiuas
+    {
+        static void Main(string[] args)
+        {
+            Kiuas kiuas1 = new Kiuas("Harvia", 0);
+
+
+
+            kiuas1.onOFF();
+
+
+
+
+            kiuas1.MuokkaaKosteutta(10);
+            kiuas1.MuokkaaLämpötilaa(120);
+            kiuas1.Tulostadata();
+            kiuas1.onOFF();
+
+
+
+
+
+
+
+        }
+    }
+}
+    
+
