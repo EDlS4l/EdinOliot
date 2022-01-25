bb1381d60703d0da9f0bce373c524924282c5bcb
3DW1Z4RD <97434008+EDlS4l@users.noreply.github.com>
2022-01-25 10:26:20 +0200

    Harjoitus 6 Eläin

---
+++ Harjoitus 6 Eläin/Kissa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tehävä6Eläin
+{
+    class Kettu : Eläin
+    {
+        private static int instanssit = 0;
+
+        public Kettu()
+        {
+            instanssit++;
+        }
+        public static new void KuinkaMonta()
+        {
+            Console.WriteLine("Kettu luotu " + instanssit);
+        }
+        public override void Ääntele()
+        {
+            Console.WriteLine("Meow Meow!");
+        }
+    }
+}
